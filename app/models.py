813b1a7d3703aa3fb80e71a91125a078bb4c9dac
Washikokevv27 <washikokevv27@gmail.com>
2020-10-21 06:39:27 +0300

    update

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
-# Where we will create handlers for error pages
+class News:
+    '''
+    News class to define News objects
+    '''
+    
+    def __init__(self,id,author,title,description,image_url,published_at):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.image_url = image_url
+        self.published_at = published_at
--- config.py
+++ config.py
@@ -1,1 +1,28 @@
-#File that will run our application
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    #NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey=18511174a077429aaaeaee76c5574ee9'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    '''
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
